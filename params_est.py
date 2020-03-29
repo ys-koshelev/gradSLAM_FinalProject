cbcf8648e6b253ec2c90cc220eeefb3285cf7fd2
keqpan <>
2020-03-29 23:25:16 +0300

    changing pose estimation to use icp for points matching

--- params_est.py
+++ params_est.py
@@ -1,6 +1,9 @@
+import open3d as o3d
+
 import torch
 import copy
 
+import numpy as np
 from utils.cloud import Cloud
 
 path_to_depth = '../stylized-ww01-plane/blender_render_depth4/1.npy'
@@ -17,7 +20,7 @@ from LM import PoseFunctionBase, DiffLM, DampingFunction
 from pose_est import PoseEst, create_rot_from_angle, transform_pc
 
 
-def align_pc(x, y, params):
+def align_pc(x, y, params, lam_min=0.1, lam_max=1, D=1, sigma=1e-5):
     """
     Fit using implemented differentiable LM
         Args:
@@ -27,31 +30,33 @@ def align_pc(x, y, params):
         Returns:
             torch.Tensor: params (translation + angle) to get y from x. Shape: (6)
     """
-#     params = torch.DoubleTensor(6).fill_(0)
+    
     s = PoseEst(x, y, init_params=params)
-    d = DampingFunction(lam_min=0.1, lam_max=1, D=1, sigma=1e-5)
-    solver = DiffLM(y=y.flatten(), function=s, decision_function=d, tol=1e-5, max_iter=3)
+    d = DampingFunction(lam_min, lam_max, D, sigma)
+    solver = DiffLM(y=y.flatten(), function=s, decision_function=d, tol=1e-7, max_iter=100)
     f = solver.optimize(verbose=False)
     return f.params
 
 
 
-pc2p = copy.copy(pc2)
-for i in range(20):
-    print("params", params)
-    transl, angle = params.split([3,3], dim=-1)
-    rot = create_rot_from_angle(angle.unsqueeze(0)).squeeze(0)
-    pc2p.points = transform_pc(pc1.points.double(), rot, transl).squeeze(1)
-    pc2p_ind = pc2p.project.round().long()
-    pc2p_valid_mask = torch.all((torch.tensor([0, 0]) <= pc2p_ind) & (pc2p_ind < torch.tensor([1920, 1080])), dim=1) # filter out
-    pc2p_ind = pc2p_ind[pc2p_valid_mask]
-    
-    mask = torch.zeros_like(pc2.depth)
-    mask[pc2p_ind[:,1],pc2p_ind[:,0]] = 1
-    depth2_mask = pc2.mask.reshape(pc2.depth.shape)
-    joint_mask = mask.bool()*depth2_mask # this mask is on the depth of camera 2
+R = pc2.extrinsic@torch.inverse(pc1.extrinsic)
+rot = R[:3,:3]
+transl = R[:3,-1]
 
-    x = pc1.points[joint_mask[pc2p_ind[:,1], pc2p_ind[:,0]]] # indexing in mask
-    mask_y = pc2p_ind[joint_mask[pc2p_ind[:,1], pc2p_ind[:,0]]]
-    y = pc2.unmasked_points.reshape(*pc2.depth.shape,3)[mask_y[:,1], mask_y[:,0]]
-    params = align_pc(x.double(), y.double(), params)
+pcA = o3d.geometry.PointCloud()
+pcA.points = o3d.utility.Vector3dVector(pc1.points)
+
+pcB = o3d.geometry.PointCloud()
+pcB.points = o3d.utility.Vector3dVector(pc2.points)
+
+
+reg_p2l = o3d.registration.registration_icp(pcA, pcB, 0.2, R)
+
+pc1_idx, pc2_idx = np.asarray(reg_p2l.correspondence_set).T
+
+x = pc1.points[pc1_idx]
+y = pc2.points[pc2_idx]
+
+params = torch.DoubleTensor(6).fill_(0)
+params = align_pc(x.double(), y.double(), params, lam_min=0.01, lam_max=1, D=1, sigma=1e-5)
+
